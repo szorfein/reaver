435bc18be4c95fe0b2fa5f732aca4a88c00375e1
szorfein <szorfein@protonmail.com>
2022-12-21 12:31:10 +0000

    add lib/download

--- lib/harpoon.rb
+++ lib/harpoon.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
+
 require_relative 'harpoon/version'
+require_relative 'harpoon/download'
+
+require 'whirly'
+require 'fileutils'
+
 module Harpoon
+  CACHE_DIR = "#{ENV['HOME']}/.cache/harpoon"
+
+  Whirly.configure spinner: 'bouncingBar',
+    color: true,
+    ambiguous_characters_width: 1
+
+  FileUtils.mkdir_p(CACHE_DIR)
 end
 
