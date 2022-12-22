a9a84da8cc35b8954ab47ebfeaaad4393337da46
szorfein <szorfein@protonmail.com>
2022-12-22 19:29:31 +0000

    add a boolean if collection files change

--- lib/reaver/collection.rb
+++ lib/reaver/collection.rb
@@ -2,6 +2,7 @@
 
 require 'yaml'
 require 'time'
+require 'digest'
 
 module Reaver
   class Collection
@@ -9,6 +10,7 @@ module Reaver
 
     def initialize(file)
       @file = file
+      @changed = false
     end
 
     def load_yaml
@@ -23,19 +25,34 @@ module Reaver
       return unless @tasks
 
       if @tasks['things'].length >= 1
-        @tasks['things'].each { |t| Reaver.download(t['url'], t['name']) }
+        @tasks['things'].each do |t|
+          if File.exists? t['name']
+            old_hash = Digest::MD5.file t['name']
+          else
+            @changed = true
+          end
+
+          Reaver.download(t['url'], t['name']) 
+          compare_hash(t['name'], old_hash) if old_hash
+        end
       end
 
       update_time
     end
 
     def save_yaml
-      return if @tasks == nil
+      return unless @tasks
 
+      @tasks['changed'] = @changed
       File.open(@file, 'w') { |f| YAML.dump(@tasks, f) }
     end
 
     private
+
+    def compare_hash(filename, old_hash)
+      hash = Digest::MD5.file filename
+      @changed = true if old_hash.hexdigest != hash.hexdigest
+    end
 
     def update_time
       return unless @tasks['things'].length >= 1
